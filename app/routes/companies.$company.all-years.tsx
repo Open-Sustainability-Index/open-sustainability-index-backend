de21d7422066dba67a05c3165a93ca869b31fc43
Henric Malmberg <henric@tiburon.se>
2024-04-18 16:12:27 +0200

    add route: /companies/$company/all-years

---
+++ app/routes/companies.$company.all-years.tsx
@@ -0,0 +1,28 @@
+import { LoaderFunctionArgs } from '@remix-run/node';
+import { fromSlug } from '../utils/fromSlug';
+import { AccessSpreadsheet } from '../services/AccessSpreadsheet';
+
+export async function loader ({ params, request }: LoaderFunctionArgs) {
+  const year = new URL(request.url).searchParams.get('year');
+  const { data: sheetData, error } = await AccessSpreadsheet();
+  const headers = sheetData?.values[0]
+  const companies = sheetData?.values.filter(value => value[2].toLowerCase() === fromSlug(params.company))
+
+  const data = companies?.map(company =>
+    company?.reduce((obj, dataPoint, index) => {
+      obj[headers[index]] = dataPoint, {}
+      return obj
+    }, {})
+  ).sort((a, b) => parseInt(b.Year) - parseInt(a.Year))
+
+  if (data?.length) {
+    return {
+      data: data,
+      error,
+    }
+  }
+
+  throw new Response("Not Found", {
+    status: 404,
+  });
+}
