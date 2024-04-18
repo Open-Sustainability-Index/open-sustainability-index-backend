0974f7c68a9d4a13889eddf6b7360bd01db29f62
Henric Malmberg <henric@tiburon.se>
2024-04-18 15:51:23 +0200

    year on /companies

--- app/routes/companies.tsx
+++ app/routes/companies.tsx
@@ -2,11 +2,13 @@ import { AccessSpreadsheet } from '../services/AccessSpreadsheet';
 
 export async function loader () {
   const { data: sheetData, error } = await AccessSpreadsheet();
+  const headers = sheetData?.values[0]
 
   const data = sheetData?.values.map(value => {
     return {
-      Name: value[2],
-      Industry: value[3],
+      [headers[1]]: value[1],
+      [headers[2]]: value[2],
+      [headers[3]]: value[3],
     }
   })
 
