7aaf5c3905ae11e92521c6d36642cdd1af9f4ede
Henric Malmberg <henric@tiburon.se>
2024-04-18 14:47:47 +0200

    add route: /companies

---
+++ app/routes/companies.tsx
@@ -0,0 +1,17 @@
+import { AccessSpreadsheet } from '../services/AccessSpreadsheet';
+
+export async function loader () {
+  const { data: sheetData, error } = await AccessSpreadsheet();
+
+  const data = sheetData?.values.map(value => {
+    return {
+      name: value[2],
+      sector: value[3],
+    }
+  })
+
+  return {
+    data,
+    error,
+  }
+}
