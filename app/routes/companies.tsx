a0cce552a4616e58f5d0a2f5d48e668b69108d68
Henric Malmberg <henric@tiburon.se>
2024-04-18 16:39:31 +0200

    add querystrings to /companies: limit, offset

--- app/routes/companies.tsx
+++ app/routes/companies.tsx
@@ -1,6 +1,11 @@
 import { AccessSpreadsheet } from '../services/AccessSpreadsheet';
 
-export async function loader () {
+export async function loader ({ request }: { LoaderFunctionArgs }) {
+  const url = new URL(request.url)
+  const limitQueryString = new URL(request.url).searchParams.get('limit')
+  const offsetQueryString = new URL(request.url).searchParams.get('offset')
+  const limit = limitQueryString ? parseInt(limitQueryString) : 0
+  const offset = offsetQueryString ? parseInt(offsetQueryString) : 0
   const { data: sheetData, error } = await AccessSpreadsheet();
   const headers = sheetData?.values[0]
 
@@ -30,7 +35,9 @@ export async function loader () {
   const deduplicatedArray = Object.values(uniqueItems);
 
   return {
-    data: deduplicatedArray,
+    data: limit ?
+      deduplicatedArray.slice(offset + 1, offset + limit + 1)
+      : deduplicatedArray.slice(1),
     error,
   }
 }
