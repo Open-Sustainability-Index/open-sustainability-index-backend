333155ddbf4add5d564f177f634b4b84fb14fd66
Henric Malmberg <henric@tiburon.se>
2024-04-25 11:16:26 +0200

    pagination on /companies

--- app/routes/companies.tsx
+++ app/routes/companies.tsx
@@ -7,12 +7,12 @@ export async function loader ({ request }: { LoaderFunctionArgs }) {
   const limitQueryString = url.searchParams.get('limit')
   const offsetQueryString = url.searchParams.get('offset')
   const nameQueryString = url.searchParams.get('name')
-  const limit = limitQueryString ? parseInt(limitQueryString) : 0
+  const limit = limitQueryString ? parseInt(limitQueryString) : 99
   const offset = offsetQueryString ? parseInt(offsetQueryString) : 0
 
   const { supabaseClient } = createSupabaseServerClient(request)
 
-  const { data, error } = await supabaseClient.from('company').select('company_name, emissions:emission("total_reported_emission_scope_1+2+3",revenue,hq_country_move,year,industry,currency),commitment(commitment_type,status,commitment_deadline),targets:target(target,type,target_year,scope)')
+  const { data, error } = await supabaseClient.from('company').select('company_name, emissions:emission("total_reported_emission_scope_1+2+3",revenue,hq_country_move,year,industry,currency),commitment(commitment_type,status,commitment_deadline),targets:target(target,type,target_year,scope)').range(offset, offset + limit)
 
   return {
     data,
