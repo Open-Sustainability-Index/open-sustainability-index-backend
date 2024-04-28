b5f9cc961aef91f11c96c2e595481e9616e92302
Henric Malmberg <henric@tiburon.se>
2024-04-28 18:58:59 +0200

    get sorted companies

--- app/routes/companies.tsx
+++ app/routes/companies.tsx
@@ -5,13 +5,22 @@ export async function loader ({ request }: { LoaderFunctionArgs }) {
   const url = new URL(request.url)
   const limitQueryString = url.searchParams.get('limit')
   const offsetQueryString = url.searchParams.get('offset')
-  const nameQueryString = url.searchParams.get('name')
-  const limit = limitQueryString ? parseInt(limitQueryString) : 99
-  const offset = offsetQueryString ? parseInt(offsetQueryString) : 0
+  const sortByQueryString = url.searchParams.get('sort')
+  const sortOrderQueryString = url.searchParams.get('order')
+
+  const limit_value = limitQueryString ? parseInt(limitQueryString) : 99
+  const offset_value = offsetQueryString ? parseInt(offsetQueryString) : 0
+  const sort_by = sortByQueryString ?? 'lower(name)'
+  const sort_order = sortOrderQueryString ?? 'asc'
 
   const { supabaseClient } = createSupabaseServerClient(request)
-
-  const { data, error } = await supabaseClient.from('company').select('company_name, emissions:emission("total_reported_emission_scope_1+2+3",revenue,hq_country_move,year,industry,currency),commitment(commitment_type,status,commitment_deadline),targets:target(target,type,target_year,scope)').range(offset, offset + limit)
+  const { data, error } = await supabaseClient
+    .rpc('companies', {
+      limit_value,
+      offset_value,
+      sort_by,
+      sort_order,
+    })
 
   return {
     data,
