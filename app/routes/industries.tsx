3cb3eb2945826bd50554abb82debf4bd010311b8
Tom Söderlund <tom@tomsoderlund.com>
2024-05-02 17:58:00 +0200

    Update industries.tsx

--- app/routes/industries.tsx
+++ app/routes/industries.tsx
@@ -2,7 +2,7 @@ import { createSupabaseServerClient } from '../supabase.server';
 import { LoaderFunctionArgs } from "@remix-run/node";
 
 export async function loader ({ request }: { LoaderFunctionArgs }) {
-  const url = new URL(request.url)
+  // const url = new URL(request.url)
   // const sort_by = url.searchParams.get('sort') ?? undefined
   // const sort_order = url.searchParams.get('order') ?? undefined
 
