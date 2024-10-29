4ed03f0f2c05a726a1ce7f5188dd5579aa981f47
Tom Söderlund <tom@tomsoderlund.com>
2024-10-29 13:02:55 +0100

    Helpful views

---
+++ supabase/migrations/20241029125636_helpful_views.sql
@@ -0,0 +1,20 @@
+-- Helpful views
+
+CREATE OR REPLACE VIEW view_companies_no_emission_data AS
+  SELECT c.name
+  FROM company c
+  LEFT JOIN emission e ON c.slug = e.company_slug
+  WHERE e.company_slug IS NULL
+  ORDER BY c.name;
+
+CREATE OR REPLACE VIEW view_countries AS
+  SELECT DISTINCT hq_country
+  FROM company
+  WHERE hq_country IS NOT NULL
+  ORDER BY hq_country;
+
+CREATE OR REPLACE VIEW view_currencies AS
+  SELECT DISTINCT UPPER(currency_local)
+  FROM emission
+  WHERE currency_local IS NOT NULL
+  ORDER BY UPPER(currency_local);
